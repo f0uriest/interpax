de28ee5f4d9c57450fb617a2d01f304b59b443e5
Kaya Unalmis <kayaunalmis@proton.me>
2025-09-14 08:32:15 -0700

    pull changes from downstream branch

--- interpax/_fourier.py
+++ interpax/_fourier.py
@@ -84,7 +84,7 @@ def ifft_interp1d(
         c = c.at[-1].divide(2)
     c = c.at[0].divide(2) * 2
 
-    x = jnp.linspace(0, dx * nx, n, endpoint=False)
+    x = jnp.linspace(0, 2 * jnp.pi, n, endpoint=False)
     x = jnp.exp(1j * (c.shape[0] // 2) * x).reshape(n, *((1,) * (c.ndim - 1)))
 
     c = _fft_pad(c, n, 0)
@@ -204,7 +204,7 @@ def ifft_interp2d(
         c = c.at[:, -1].divide(2)
     c = c.at[:, 0].divide(2) * 2
 
-    y = jnp.linspace(0, dy * ny, n2, endpoint=False)
+    y = jnp.linspace(0, 2 * jnp.pi, n2, endpoint=False)
     y = jnp.exp(1j * (c.shape[1] // 2) * y).reshape(1, n2, *((1,) * (c.ndim - 2)))
 
     c = jnp.fft.ifft(c, axis=0, norm="forward")
